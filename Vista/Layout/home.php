7212c4afe1d8e0bbf1a478545acbedfaedf28be1
Joseline Cisternas <Joseline Cisternas@Joseline>
2016-06-26 17:24:45 -0400

    con editar a medias dos

--- Vista/Layout/home.php
+++ Vista/Layout/home.php
@@ -19,32 +19,41 @@ $perfil = $_SESSION["idPerfil"];
         <link href="../../Files/Complementos/bootstrap/css/bootstrap-flat.css" rel="stylesheet">        
         <link href="../../Files/Complementos/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link  href="../../Files/css/style.css" rel="stylesheet">
-
         <link  href="../../Files/css/chat.css" rel="stylesheet">  
         <link href="../../Files/css/chat.plugin.css" rel="stylesheet">
         <link  href="../../Files/js/charts/jquery.easy-pie-chart.css" rel="stylesheet">  
         <link  href="../../Files/Complementos/lib/selectize/selectize.css" rel="stylesheet">  
         <link  href="../../Files/css/component.css" rel="stylesheet"> 
         <link  href="../../Files/css/style-responsive.css" rel="stylesheet">
-
-
         <script src="../../Files/js/jquery.js"></script>
         <script src="../../Files/Complementos/lib/selectize/selectize.js"></script>        
         <script src="../../Files/Complementos/bootstrap/js/bootstrap.min.js"></script>        
-
         <script src="../../Files/js/charts/jquery.sparkline.min.js?v1.4.0"></script>
         <script src="../../Files/js/charts/jquery.easy-pie-chart.js?v1.4.0"></script>
         <script src="../../Files/js/charts/raphael.2.1.0.min.js?v1.4.0"></script>
         <script src="../../Files/js/charts/justgage.1.0.1.min.js?v1.4.0"></script>
-
         <script src="../../Files/Complementos/lib/scroll-slim/jquery.slimscroll.min.js"></script>
         <script src="../../Files/js/common.js"></script>
+
+        <!--Carrusel-->
+        <link rel="shortcut icon"  href="files/img/icono.png" sizes="16x16">        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
         <script>
             $('.carousel').carousel({
                 interval: 3000
             })
         </script>
+        <!--Fin import carrusel-->
+
 
     </head>
     <body >
@@ -111,9 +120,51 @@ $perfil = $_SESSION["idPerfil"];
                                     </div>
                                     <div class="body" style="text-align: center;">
                                         <div class="row-fluid">
-                                            <!-- CONTENIDO AQUI -->
-                                            <div><img src="../../Files/img/content/file.jpg"></div>
+                                            <!--Carrusel-->
+                                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                                <!-- Indicators --> 
+                                                <ol class="carousel-indicators">
+                                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                                                </ol>
+                                                <!-- Wrapper for slides -->
+                                                <div class="carousel-inner">
+                                                    <div class="item active">
+                                                        <img src="../../Files/img/b4.jpg" alt="...">
+                                                        <div class="carousel-caption">
+                                                            <h3></h3>
+                                                        </div>
+                                                    </div>
+                                                    <div class="item">
+                                                        <img src="../../Files/img/b2.jpg" alt="...">
+                                                        <div class="carousel-caption">
+                                                            <h3></h3>
+                                                        </div>
+                                                    </div>
+                                                    <div class="item">
+                                                        <img src="../../Files/img/b3.jpg" alt="...">
+                                                        <div class="carousel-caption">
+                                                            <h3></h3>
+                                                        </div>
+                                                    </div>
+                                                    <div class="item">
+                                                        <img src="../../Files/img/b1.jpg" alt="...">
+                                                        <div class="carousel-caption">
+                                                            <h3></h3>
+                                                        </div>
+                                                    </div>
+                                                </div>       
 
+                                                <!-- Controls -->
+                                                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                                </a>
+                                                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                                </a>
+                                            </div> <!-- Carousel -->
                                         </div>
                                     </div>
                                 </div>
